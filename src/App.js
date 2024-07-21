a97a107404c5d8b388f74246c14c3f53fbff2632
peterghatas <peter1ghatas@gmail.com>
2024-07-21 23:06:40 +0300

    second

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello world</h1>
+        <h1>خد بوسة يا زوز</h1>
       </header>
     </div>
   );
